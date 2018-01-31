bf79b13954cdca5f880471752ff92795cc8faff5
dschmidt42 <dschmidt42.ext@it4ipm.de>
2018-01-31 15:56:07 +0100

    realize double jab and double cross (no 100% accuracy yet)

--- Assets/LeapMotion/Core/Scripts/Attachments/Sample.cs
+++ Assets/LeapMotion/Core/Scripts/Attachments/Sample.cs
@@ -9,6 +9,8 @@ public class Sample : MonoBehaviour {
 	private Controller controller;
 	private Hand leftHand;
 	private Hand rightHand;
+	private Hand rightHandPreviousFrame;
+	private Hand leftHandPreviousFrame;
 
 
 	// Use this for initialization
@@ -25,30 +27,65 @@ public class Sample : MonoBehaviour {
 			Frame frame = controller.Frame (); //The latest frame
 			Frame previous = controller.Frame (1); //The previous frame
 
-			if(frame.Hands.Count > 1){
-				List<Hand> hands = frame.Hands;
-				if (hands[0].IsLeft){
-					leftHand = hands [0];
-					rightHand = hands [1];
-				} else {
-					rightHand = hands [0];
-					leftHand = hands [1];
-				}
 
-			
+			getHandsfromFrame (frame);
+			getHandsfromPreviousFrame (previous);
 					
-				if (Math.Abs (Math.Abs(leftHand.PalmPosition.z) - Math.Abs((rightHand.PalmPosition.z))) > 80.0f){
-//					Debug.Log ("Unterschied " + Math.Abs (Math.Abs (leftHand.PalmPosition.z) - Math.Abs ((rightHand.PalmPosition.z))));
+			if (punchIsThrown(leftHand, rightHand) && !punchIsThrown (leftHandPreviousFrame, rightHandPreviousFrame) && (punchForwardLeft() || punchForwardRight ())) {
 					if (leftHand.PalmPosition.z < rightHand.PalmPosition.z) {
 						Debug.Log ("JAB!!!");
+						return;
 					} else {
 						Debug.Log ("CROSS!!!");
+						return;
 					}
 				} 
 					
-			}
+
 				
 		}
+	}
+
+	bool punchIsThrown (Hand _lefthand, Hand _righthand)
+	{
+		return Math.Abs (Math.Abs (_lefthand.PalmPosition.z) - Math.Abs ((_righthand.PalmPosition.z))) > 80.0f;
+	}
+
+
+	void getHandsfromFrame(Frame frame){
+		if(frame.Hands.Count > 1){
+			List<Hand> hands = frame.Hands;
+			if (hands[0].IsLeft){
+				leftHand = hands [0];
+				rightHand = hands [1];
+			} else {
+				rightHand = hands [0];
+				leftHand = hands [1];
+			}
+		}
+
+	}
+
+	void getHandsfromPreviousFrame(Frame frame){
+		if(frame.Hands.Count > 1){
+			List<Hand> hands = frame.Hands;
+			if (hands[0].IsLeft){
+				leftHandPreviousFrame = hands [0];
+				rightHandPreviousFrame = hands [1];
+			} else {
+				rightHandPreviousFrame = hands [0];
+				leftHandPreviousFrame = hands [1];
+			}
+		}
+
+	}
+
+	bool punchForwardLeft() {
+		return leftHandPreviousFrame.PalmPosition.z > leftHand.PalmPosition.z;
+}
+
+	bool punchForwardRight(){
+		return rightHandPreviousFrame.PalmPosition.z > rightHand.PalmPosition.z;;
 	}
 
 }
