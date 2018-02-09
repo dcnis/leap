aba264dba9290f74e278e75b3d922acb301c2263
dschmidt42 <dschmidt42.ext@it4ipm.de>
2018-02-09 16:03:22 +0100

    GOOD VERSION: Combination will be stored and deleted after 2seconds of doing nothing

--- Assets/LeapMotion/Core/Scripts/Attachments/Sample.cs
+++ Assets/LeapMotion/Core/Scripts/Attachments/Sample.cs
@@ -18,7 +18,7 @@ public class Sample : MonoBehaviour
 	private String punchCombination = "";
 	private bool isCombinationCreating = false;
 	private bool firstPunchThrown = false; 
-
+	private bool isRunning = false;
 	// Use this for initialization
 	void Start ()
 	{
@@ -39,8 +39,21 @@ public class Sample : MonoBehaviour
 				foreach (Hand hand in hands) {
 					this.evaluatePunch (hand);
 				}
-					
+
+				if (punch == "none") {
+					if (!isRunning) {
+						StartCoroutine ("IsCombinationDone");
+					}
+				}
+
+
+
+
 			}
+
+
+
+
 		}
 	}
 
@@ -54,41 +67,56 @@ public class Sample : MonoBehaviour
 		if (hand.PalmVelocity.Dot (new Vector (0, 0, -1)) > PALM_Z_VELOCITY) {
 			if (hand.IsLeft) {
 				punch = LEFT_PUNCH;
-				Combination.thrown = this.punch;
 			} else {
 				punch = RIGHT_PUNCH;
-				Combination.thrown = this.punch;
 			}
-//			this.punchCombination = string.Concat (this.punchCombination, punch);
-			Debug.Log (punch);
+			this.punchCombination = string.Concat (this.punchCombination, punch);
+			Debug.Log ("My PunchCombination: " +  punchCombination);
 			return;
 		}
 
 	}
 
 
-	private void sendCombination ()
+	private void sendCombination (string punchCombo)
 	{
-//		if (punch != "none") {
-//			return;
-//		}
-//		this.punchCombination = "";
-//		this.isCombinationCreating = true;
-//		this.punchCombination = string.Concat (this.punchCombination, punchCombo [0]);
-//
-//		for (int i = 0; i < punchCombo.Length; i++) {
-//			if (punchCombo.Length > 2) {
-//				if (i == punchCombo.Length){break;}
-//				if (punchCombo [i] != punchCombo [i + 1]) {
-//					this.punchCombination = string.Concat (this.punchCombination, punchCombo [i + 1]);
-//				}
-//			}
-//		}
-//
-		Debug.Log ("PUNCH THROWN IS FOLLOWING: " + this.punch);
+		if (punch != "none") {
+			return;
+		}
 
-}
+		this.isCombinationCreating = true;
+		this.combination = "";
+		this.combination = string.Concat (this.combination, punchCombo [0]);
+
+		for (int i = 0; i < punchCombo.Length; i++) {
+			if (punchCombo.Length > 2) {
+				if (i == punchCombo.Length){break;}
+				if (punchCombo [i] != punchCombo [i + 1]) {
+					this.combination = string.Concat (this.combination, punchCombo [i + 1]);
+				}
+			}
+		}
+
+		Debug.Log ("PUNCH THROWN IS FOLLOWING: " + this.combination);
+		Combination.thrown = this.combination;
+
+		this.isCombinationCreating = false;
+	}
 		
+
+	IEnumerator IsCombinationDone() {
+		isRunning = true;
+		if(punch == "none"){
+			yield return new WaitForSeconds(2);
+		}
+		if (punch == "none"){
+			this.punchCombination = "";
+			Debug.Log ("I DELETED IT!!!!!");
+		}
+		isRunning = false;
+
+	}
+
 
 
 }
